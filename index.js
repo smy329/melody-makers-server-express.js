506206bf1d35e8306d4989b04818e3f7f1ba6cb7
Syed Mohammad Yasir <smy329@gmail.com>
2023-06-07 02:26:52 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+/**after running node on terminal, type "require('crypto').randomBytes(64).toString('hex')". In this way, you can generate 64 byte ACCESS_TOKEN_SECRET key  */
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//melodyMakersCampDB
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcfhua9.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  // Set a cookie named 'myCookie' with the value 'example'
+  res.cookie('melody-makers-camp-cookie', 'I am here');
+  res.send('Hello from, Melody Maker Camp. Check your cookie to get your gift');
+});
+
+app.listen(port, () => {
+  console.log(`Running at port: ${port}`);
+});
