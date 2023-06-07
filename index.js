cec65eda75067bd08fd483febd32b77dc92decc0
Syed Mohammad Yasir <smy329@gmail.com>
2023-06-07 14:00:13 +0600

    creating account api

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//melodyMakersCampDB
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorized Access' });
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'Invalid Token' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcfhua9.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,7 +42,52 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    //await client.connect();
+    await client.connect();
+
+    const usersCollection = client.db('melodyMakersCampDB').collection('users');
+
+    //jwt
+    app.post('/jwt', (req, res) => {
+      const userEmail = req.body;
+      const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      console.log(token);
+      res.send({ token });
+    });
+
+    //middleware verifyAdmin: this code must be excuted after database connection. For finding admin we need  database.
+    // must be write this middleware after verifyJWT
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'Forbidden Access' });
+      }
+
+      //next() means you can proceed
+      next();
+    };
+
+    //users related api
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      //console.log(user);
+      const query = { email: user.email };
+      //console.log(query);
+      const existingUser = await usersCollection.findOne(query);
+      //console.log(existingUser);
+      if (existingUser) {
+        return res.send({ message: 'User already exists ' });
+      }
+      const result = await usersCollection.insertOne(user);
+      //console.log(result);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
